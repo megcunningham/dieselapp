fbc45da819c0a40cc2aa0a05babd6b224ec37c91
Meg Cunningham <meg_cunningham@live.com>
2015-07-29 13:35:51 -0500

    login and index page working

---
+++ mydieselapp/www/js/config.js
@@ -0,0 +1,49 @@
+angular
+  .module('dieselApp')
+
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+       .state('login', {
+          url: "/login",
+            templateUrl:'templates/login-reg.html',
+            controller: 'AuthCtrl',
+            controllerAs: 'auth'
+      })
+      .state('index', {
+        url: "/index",
+        templateUrl: 'templates/trainingIndex.html',
+        controller: 'trainingIndexCtrl'
+      })
+
+      .state('armPage', {
+        url: "/arms",
+        templateUrl:'templates/armPage.html',
+        controller: 'ArmCtrl',
+        controllerAs: 'arm'
+       })
+
+      // .when('/backlist', {
+      //   templateUrl:'www/templates/backPage.html',
+      //   controller: 'BackCtrl',
+      //   controllerAs: 'back'
+      // })
+
+      // .when('/chestlist', {
+      //   templateUrl:'www/templates/chestPage.html',
+      //   controller: 'ChestCtrl',
+      //   controllerAs: 'chest'
+      // })
+
+      // .when('/leglist', {
+      //   templateUrl:'www/templates/legPage.html',
+      //   controller: 'LegCtrl',
+      //   controllerAs: 'leg'
+      // })
+
+      // .when('/shoulderlist', {
+      //   templateUrl:'www/templates/shoulderPage.html',
+      //   controller: 'ShoulderCtrl',
+      //   controllerAs: 'shoulder'
+      // });
+
+    });
